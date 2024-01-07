9b09d3b6f684c49a19124794319ec6c2c4ad0e79
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-07 12:59:41 +0000

    setup clerk theme with next theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import type { PropsWithChildren } from "react";
+import { type PropsWithChildren } from "react";
 
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
@@ -32,17 +32,15 @@ const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ConvexClientProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-            storageKey="jotion-theme"
-          >
-            {children}
-          </ThemeProvider>
-        </ConvexClientProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="jotion-theme"
+        >
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -1,16 +1,23 @@
 "use client";
 
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { dark } from "@clerk/themes";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
-import type { PropsWithChildren } from "react";
+import { type PropsWithChildren } from "react";
+import { useTheme } from "next-themes";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export const ConvexClientProvider = ({ children }: PropsWithChildren) => {
+  const { resolvedTheme } = useTheme();
+
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+      appearance={{
+        baseTheme: resolvedTheme === "dark" ? dark : undefined,
+      }}
     >
       <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
         {children}
