eec3e97803be52af1a5e9e7986ccde3df3cf0d38
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-07 11:30:58 +0000

    clerk auth config created

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://infinite-titmouse-77.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
