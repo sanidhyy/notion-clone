90953bcb7c8dea5b1100487e5f2a1bdcdc39c240
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-07 17:14:32 +0000

    custom item component ui layout created

---
+++ app/(main)/_components/item.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { type Id } from "@/convex/_generated/dataModel";
+import { ChevronDown, ChevronRight, type LucideIcon } from "lucide-react";
+
+type ItemProps = {
+  id?: Id<"documents">;
+  documentIcon?: string;
+  active?: boolean;
+  expanded?: boolean;
+  isSearch?: boolean;
+  level?: number;
+  onExpand?: () => void;
+  label: string;
+  onClick: () => void;
+  icon: LucideIcon;
+};
+
+export const Item = ({
+  id,
+  label,
+  onClick,
+  icon: Icon,
+  active,
+  documentIcon,
+  isSearch,
+  level = 0,
+  onExpand,
+  expanded,
+}: ItemProps) => {
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
+  return (
+    <div
+      role="button"
+      onClick={onClick}
+      style={{
+        paddingLeft: level ? `${level * 12 + 12}px` : "12px",
+      }}
+      className="group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
+    >
+      <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      <span className="truncate">{label}</span>
+    </div>
+  );
+};
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,20 +1,23 @@
 "use client";
 
-import { useQuery } from "convex/react";
-import { ChevronsLeft, Menu } from "lucide-react";
+import { useMutation, useQuery } from "convex/react";
+import { ChevronsLeft, Menu, PlusCircle } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { type ElementRef, useRef, useState, useEffect } from "react";
+import { toast } from "sonner";
 import { useMediaQuery } from "usehooks-ts";
 
 import { api } from "@/convex/_generated/api";
 import { cn } from "@/lib/utils";
 
+import { Item } from "./item";
 import { UserItem } from "./user-item";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const documents = useQuery(api.documents.get);
+  const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -84,6 +87,16 @@ export const Navigation = () => {
     setTimeout(() => setIsResetting(false), 300);
   };
 
+  const handleCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created.",
+      error: "Failed to create a new note.",
+    });
+  };
+
   useEffect(() => {
     if (isMobile) collapse();
     else resetWidth();
@@ -115,6 +128,7 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
           {documents?.map((document) => (
