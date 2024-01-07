96699bc3df0962c2b399e11c6a6732f70c3d993b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-07 16:54:36 +0000

    note create function implement

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
 import { PlusCircle } from "lucide-react";
 import Image from "next/image";
+import { toast } from "sonner";
+
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
 
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" }); // default note title will be Untitled
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created.",
+      error: "Failed to create a new note.",
+    });
+  };
 
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -28,7 +43,7 @@ const DocumentsPage = () => {
       <h2 className="text-lg font-medium">
         Welcome to {user?.firstName}&apos;s Jotion.
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
         Create a note
       </Button>
