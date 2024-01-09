7b0bcb58fb4a8ce8cf87db91e62d4db0bc7e6917
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-09 10:04:43 +0000

    documents api route implemented

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,13 +3,25 @@ import { v } from "convex/values";
 import { Doc, Id } from "./_generated/dataModel";
 import { mutation, query } from "./_generated/server";
 
-export const get = query({
-  handler: async (ctx) => {
+export const getSidebar = query({
+  args: {
+    parentDocument: v.optional(v.id("documents")),
+  },
+  handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
 
-    if (!identity) throw new Error("Not authenticated.");
+    if (!identity) throw new Error("Unauthenticated.");
 
-    const documents = await ctx.db.query("documents").collect();
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user_parent", (q) =>
+        q.eq("userId", userId).eq("parentDocument", args.parentDocument)
+      )
+      .filter((q) => q.eq(q.field("isArchived"), false))
+      .order("desc")
+      .collect();
 
     return documents;
   },
@@ -23,7 +35,7 @@ export const create = mutation({
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
 
-    if (!identity) throw new Error("Not authenticated.");
+    if (!identity) throw new Error("Unauthenticated.");
 
     const userId = identity.subject;
 
