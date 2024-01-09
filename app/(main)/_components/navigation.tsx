e9da8d8cd4f8b22eb596af6a36ea48674733b378
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-09 12:06:53 +0000

    opened settings modal on click

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -22,6 +22,7 @@ import {
 } from "@/components/ui/popover";
 import { api } from "@/convex/_generated/api";
 import { useSearch } from "@/hooks/use-search";
+import { useSettings } from "@/hooks/use-settings";
 import { cn } from "@/lib/utils";
 
 import { DocumentList } from "./document-list";
@@ -30,6 +31,7 @@ import { TrashBox } from "./trash-box";
 import { UserItem } from "./user-item";
 
 export const Navigation = () => {
+  const settings = useSettings();
   const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -145,7 +147,7 @@ export const Navigation = () => {
         <div>
           <UserItem />
           <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
-          <Item label="Settings" icon={Settings} onClick={() => {}} />
+          <Item label="Settings" icon={Settings} onClick={settings.onOpen} />
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
