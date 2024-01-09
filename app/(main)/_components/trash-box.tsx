915489774735c871eeb277a153c8d9a7f70bde1b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-09 12:08:08 +0000

    fix trash box dark mode

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -91,7 +91,7 @@ export const TrashBox = () => {
               <div
                 role="button"
                 onClick={(e) => onRestore(e, document._id)}
-                className="rounded-sm p-2 hover:bg-neutral-200"
+                className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
               >
                 <Undo className="h-4 w-4 text-muted-foreground" />
               </div>
@@ -99,7 +99,7 @@ export const TrashBox = () => {
               <ConfirmModal onConfirm={() => onRemove(document._id)}>
                 <div
                   role="button"
-                  className="rounded-sm p-2 hover:bg-neutral-200"
+                  className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
                 >
                   <Trash className="h-4 w-4 text-muted-foreground" />
                 </div>
