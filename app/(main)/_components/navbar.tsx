f8a98a36525071025fd3be826a10526ecaba65c4
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-09 12:53:09 +0000

    navbar implemented

---
+++ app/(main)/_components/navbar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useQuery } from "convex/react";
+import { MenuIcon } from "lucide-react";
+import { useParams } from "next/navigation";
+
+import { api } from "@/convex/_generated/api";
+import type { Id } from "@/convex/_generated/dataModel";
+
+import { Title } from "./title";
+
+type NavbarProps = {
+  isCollapsed: boolean;
+  onResetWidth: () => void;
+};
+
+export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
+  const params = useParams();
+
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId as Id<"documents">,
+  });
+
+  if (document === undefined)
+    return (
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+        <Title.Skeleton />
+      </nav>
+    );
+
+  if (document === null) return null;
+
+  return (
+    <>
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
+        {isCollapsed && (
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="h-6 w-6 text-muted-foreground"
+          />
+        )}
+
+        <div className="flex items-center justify-between w-full">
+          <Title initialData={document} />
+        </div>
+      </nav>
+    </>
+  );
+};
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -10,7 +10,7 @@ import {
   Settings,
   Trash,
 } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { type ElementRef, useRef, useState, useEffect } from "react";
 import { toast } from "sonner";
 import { useMediaQuery } from "usehooks-ts";
@@ -27,12 +27,14 @@ import { cn } from "@/lib/utils";
 
 import { DocumentList } from "./document-list";
 import { Item } from "./item";
+import { Navbar } from "./navbar";
 import { TrashBox } from "./trash-box";
 import { UserItem } from "./user-item";
 
 export const Navigation = () => {
   const settings = useSettings();
   const search = useSearch();
+  const params = useParams();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -183,15 +185,19 @@ export const Navigation = () => {
           isMobile && "left-0 w-full"
         )}
       >
-        <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && (
-            <Menu
-              onClick={resetWidth}
-              role="button"
-              className="h-6 w-6 text-muted-foreground"
-            />
-          )}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+        ) : (
+          <nav className="bg-transparent px-3 py-2 w-full">
+            {isCollapsed && (
+              <Menu
+                onClick={resetWidth}
+                role="button"
+                className="h-6 w-6 text-muted-foreground"
+              />
+            )}
+          </nav>
+        )}
       </div>
     </>
   );
