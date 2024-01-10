8fb2563d3934c5213ecf1e79fd4ebfee9ec44d13
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-10 09:16:37 +0000

    site config added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,31 +5,15 @@ import { type PropsWithChildren } from "react";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ToasterProvider } from "@/components/providers/toaster-provider";
+import { siteConfig } from "@/config";
 import { EdgeStoreProvider } from "@/lib/edgestore";
 
 import "./globals.css";
-import { ToasterProvider } from "@/components/providers/toaster-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Jotion",
-  description: "The connected workspace where better, faster work happens.",
-  icons: {
-    icon: [
-      {
-        media: "(prefers-color-scheme: light)",
-        url: "/logo.svg",
-        href: "/logo.svg",
-      },
-      {
-        media: "(prefers-color-scheme: dark)",
-        url: "/logo-dark.svg",
-        href: "/logo-dark.svg",
-      },
-    ],
-  },
-};
+export const metadata: Metadata = siteConfig;
 
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
