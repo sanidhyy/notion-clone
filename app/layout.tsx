0af5afdd78057987139af3b4ecc1498190a888f5
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-10 09:22:04 +0000

    theme color updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import { type PropsWithChildren } from "react";
 
@@ -14,6 +14,13 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = siteConfig;
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#1F1F1F" },
+    { media: "(prefers-color-scheme: light)", color: "#EFEFEF" },
+  ],
+};
 
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
