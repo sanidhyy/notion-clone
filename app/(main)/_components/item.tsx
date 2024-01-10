f76b0b61438d17281f1a373eed4ffebd8ff9cad4
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-10 08:05:32 +0000

    fix item alignment in sidebar

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -124,7 +124,7 @@ export const Item = ({
           {documentIcon} {/* Emoji */}
         </div>
       ) : (
-        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+        <Icon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
       )}
 
       <span className="truncate">{label}</span>
